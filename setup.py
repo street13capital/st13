ff51baf14565f00bb5c9b764be21c2db40615cb9
Ken Soh <kensoh@gmail.com>
2025-05-16 12:20:23 +0100

    initial version for pypi.org

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='st13',
+    version='0.3.0',
+    py_modules=['st13'],
+    author='Ken Soh',
+    license='MIT License',
+    url='https://github.com/street13capital/st13',
+    description='Python package for trend analysis',
+    long_description='Python package for trend analysis using technical analysis of price behaviour around trend lines',
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
---
+++ st13.py
@@ -0,0 +1,4 @@
+"""Python package for trend analysis"""
+# MIT License, Copyright 2025 Street 13 Capital Ltd
+# https://github.com/street13capital/st13/blob/main/LICENSE
+__version__ = '0.3.0'

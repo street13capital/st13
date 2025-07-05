a21842d3642882a9da7d234bf4e60b08d1471d7f
Ken Soh <kensoh@gmail.com>
2025-07-05 01:57:00 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='st13',
-    version='0.6.0',
+    version='0.7.0',
     py_modules=['st13'],
     install_requires=['pandas',
                       'yfinance',

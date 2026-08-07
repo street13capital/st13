c057505b61e129cfad9521021779587fbb846e0e
Ken Soh <kensoh@gmail.com>
2026-08-07 18:08:34 +0800

    st13.py - bump up year to 2026

--- st13.py
+++ st13.py
@@ -1,5 +1,5 @@
 """Python package for trend analysis"""
-# MIT License, Copyright 2025 Street 13 Capital Ltd
+# MIT License, Copyright 2026 Street 13 Capital Ltd
 # https://github.com/street13capital/st13/blob/main/LICENSE
 __version__ = '0.9.1'
 
